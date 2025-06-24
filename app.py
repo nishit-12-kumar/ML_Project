9408102fb61a359e87fd5f23790a539fe6cbdc58
Nishit Kumar <nishitkumaroll12@gmail.com>
2025-06-24 12:19:08 +0530

    Made Home1 , result and updated UI

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
 @app.route('/predictdata', methods= ['GET' , 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
-        return  render_template('home.html')
+        return  render_template('home1.html')
     else:
         data=CustomData(
             gender=request.form.get('gender'),
@@ -37,7 +37,7 @@ def predict_datapoint():
 
         predict_pipeline= PredictPipeline()
         results= predict_pipeline.predict(pred_df)
-        return render_template('home.html', results=results[0])
+        return render_template('result.html', results=results[0])
     
 
 
